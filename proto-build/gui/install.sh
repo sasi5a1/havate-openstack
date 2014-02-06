2a3f24f184048be7e291e6cf4d7ee7f16ea03788
Robert Starmer <rstarmer@gmail.com>
2014-02-06 15:29:15 -0800

    Change to 8443 port for gui

--- proto-build/gui/install.sh
+++ proto-build/gui/install.sh
@@ -4,7 +4,9 @@ python manage.py syncdb --noinput
 python manage.py migrate
 chmod 660 /gui/openstack.db
 chown root.www-data /gui/openstack.db
-ln -s /gui/gui.conf /etc/apache2/conf.d/gui.conf
+#Only deploying the GUI
+#ln -s /gui/gui.conf /etc/apache2/conf.d/gui.conf
+ln -s /gui/gui-8443.conf /etc/apache2/conf.d/gui.conf
 ln -s /gui/mirror.conf /etc/apache2/conf.d/mirror.conf
 cd /gui/UcsSdk-0.5
 python setup.py install
