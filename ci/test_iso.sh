19645d19dfab8f5183b3990e4f5b1c2311892e0c
Robert Starmer <rstarmer@gmail.com>
2014-02-15 02:16:13 -0800

    Add updates to ci test

--- ci/test_iso.sh
+++ ci/test_iso.sh
@@ -9,6 +9,8 @@ rm /var/lib/libvirt/images/staging.img
 dd if=/dev/zero of=/var/lib/libvirt/images/staging.img bs=1G count=12
 virsh create ~/staging.xml
 
+until [ $(curl -s http://172.16.0.5/finished | grep up) ]; do echo 'waiting...'>>/tmp/test.log ; sleep 5 ; done
+
 virsh destroy pxe
 virsh undefine pxe
 rm /var/lib/libvirt/images/pxe.img
