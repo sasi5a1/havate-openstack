46667db202aadb3fff221a7467260207d40da1ec
Robert Starmer <rstarmer@gmail.com>
2014-04-04 14:55:18 -0700

    Update test script to provide better feedback

--- ci/test_iso.sh
+++ ci/test_iso.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-set -x
 
 /root/staging.sh
+ssh-keygen -R 192.168.241.240
+ssh-keygen -R 192.168.241.230
 
 virsh destroy staging
-virsh undefine staging
 rm /var/lib/libvirt/images/staging.img
 dd if=/dev/zero of=/var/lib/libvirt/images/staging.img bs=1G count=16
 virsh create ~/staging.xml
-
-until [ $(curl -s http://192.168.241.240/ubuntu/finished | grep up) ]; do echo 'waiting...'>>/tmp/test.log ; sleep 5 ; done
+count=0
+until [ $(curl -s http://192.168.241.240/ubuntu/finished | grep up) ]; do echo "waiting... try ${count}" |& tee /tmp/test.log ; ((count++)); sleep 15 ; done
 
 virsh destroy pxe
-virsh undefine pxe
 rm /var/lib/libvirt/images/pxe.img
 dd if=/dev/zero of=/var/lib/libvirt/images/pxe.img bs=1G count=16
 virsh create ~/pxe.xml
